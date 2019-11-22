64c4e34241ca8b9771ac7258b7840fa3b8a64e0f
Edwin <Edwin.Martinez@people10.com>
2019-11-22 11:50:31 -0500

    Initial commit of my components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from './components/header';
+
+import './app.scss';
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
