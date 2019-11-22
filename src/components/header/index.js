3d53291e3a69afd87ed22dca373b0c4b85fc6338
Edwin <Edwin.Martinez@people10.com>
2019-11-22 13:25:11 -0500

    Header Component and Tests

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,10 +5,10 @@ import './styles.scss';
 
 const Header = (props) => {
     return (
-        <header>
+        <header data-test="headerComponent" >
             <div className="wrap">
                 <div className="logo">
-                    <img src={Logo} alt="Logo" />
+                    <img data-test="logoIMG" src={Logo} alt="Logo" />
                 </div>
             </div>
         </header>
